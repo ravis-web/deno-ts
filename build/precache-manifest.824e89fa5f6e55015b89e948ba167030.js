b3cee799b844ab347580b31b4dc49931666a64fe
Ravi Shankar <67696330+ravis-web@users.noreply.github.com>
2020-07-31 21:12:05 +0530

    deno : react-build
    - added react-build for frontend

---
+++ build/precache-manifest.824e89fa5f6e55015b89e948ba167030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532b68474df66c84bbe5bbb9dfbf3756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bca074fd2075563fd24",
+    "url": "/static/css/main.a7cedaa9.chunk.css"
+  },
+  {
+    "revision": "3041a4d33ac86c08ee72",
+    "url": "/static/js/2.37ccc9f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37ccc9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bca074fd2075563fd24",
+    "url": "/static/js/main.ff870f78.chunk.js"
+  },
+  {
+    "revision": "4db7e5538013c31b493a",
+    "url": "/static/js/runtime-main.e7d70c93.js"
+  }
+]);
